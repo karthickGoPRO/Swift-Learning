2088248f702aaf8d1c12f04b7e3b1a4eebb67971
KarthickGoPRO <karthickgopro@gmail.com>
2024-11-04 13:04:51 +0530

    Initial Commit

---
+++ Swift_Learning_App/Swift_Learning_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift_Learning_App
+//
+//  Created by Karthick Thavasimuthu on 04/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Swift_Learning_App/Swift_Learning_AppTests/Swift_Learning_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Learning_AppTests.swift
+//  Swift_Learning_AppTests
+//
+//  Created by Karthick Thavasimuthu on 04/11/24.
+//
+
+import Testing
+@testable import Swift_Learning_App
+
+struct Swift_Learning_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
